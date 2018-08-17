a17e9b21a0f6fa88d3688fdb047916771f5c6eb7
Jason Viger <jason.viger@gmail.com>
2018-08-17 05:47:14 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,8 @@ require 'pry'
 def remove_strawberry(contacts)
 
 contacts.each do |person, contact_details_hash|
-  binding.pry 
+  contacts_deatils_hash.each do |attribute, data|
+    binding.pry
+  end
 end
 end
